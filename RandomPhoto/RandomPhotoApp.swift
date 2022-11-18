c1f23359df21627fa5431a5fbeda451751363c99
NatalieSneed <106278703+NatalieSneed@users.noreply.github.com>
2022-11-17 19:27:37 -0700

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Natalie Sneed on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
